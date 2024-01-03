4915a71ecfeb5252d3871bae344f5da399f4a92a
Eugene Zabolotny <daseugene@dasugene-mac.local>
2024-01-03 16:29:48 +0300

    ..

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import sqlite3
+import repositories
 
 
+while True:
+
+    print("1. Get your tasks \n 2. Create new task \n")
+
+    move = int(input("Choose an action(type a number): "))
+    # title = input("Print a title of your task: ")
+    if move == 1:
+        return.get_tasks()
---
+++ repositories.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+con = sqlite3.connect("todo.db")
+
+cur = con.cursor()
+
+async def create_table():
+    cur.execute("CREATE TABLE tasks(title, description, done)")
+
+
+async def get_tasks():
+    return(cur.execute("SELECT * from tasks"))
+
+
+
